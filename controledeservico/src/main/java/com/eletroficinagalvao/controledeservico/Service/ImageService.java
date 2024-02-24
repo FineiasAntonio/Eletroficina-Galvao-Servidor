f27e97227b11f6fcb69c57f9de9165a0411fbb18
FineiasAntonio <fineias1187@gmail.com>
2024-02-23 22:34:03 -0300

    :sparkles: feat: configuração inicial do firebase e upload de imagem

--- controledeservico/src/main/java/com/eletroficinagalvao/controledeservico/Service/ImageService.java
+++ controledeservico/src/main/java/com/eletroficinagalvao/controledeservico/Service/ImageService.java
@@ -1,23 +1,24 @@
 package com.eletroficinagalvao.controledeservico.Service;
 
-import com.eletroficinagalvao.controledeservico.Exception.NotFoundException;
 import com.eletroficinagalvao.controledeservico.Repository.OSRepository;
-import com.eletroficinagalvao.controledeservico.Util.AppProperties;
+import com.google.auth.Credentials;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.sql.SQLDataException;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
 @Service
 public class ImageService {
@@ -31,45 +32,51 @@ public class ImageService {
 
     private Random random = new Random();
 
-    //private static String diretorio = "C:/Users/%s/Pictures".formatted(System.getProperty("user.name"));
-    private static String diretorio = AppProperties.get("storage.path");
-
     @Autowired
     private OSRepository repository;
 
-    public String readImage(int id, List<MultipartFile> imagens, int method) {
+    public String uploadImage(int id, List<MultipartFile> imagens, int method) {
         setMethod(method);
 
         try {
+            InputStream credentialsStream = ImageService.class.getClassLoader().getResourceAsStream("eletroficina-galvao-storage-firebase-adminsdk-31szq-ab6423537a.json");
+            Credentials credentials = GoogleCredentials.fromStream(credentialsStream);
+            Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
 
-            diretorioUsuario = Files.createDirectories(Path.of(diretorio + "\\OrdensDeServicos\\ImagensOS\\%d\\%s".formatted(id, getMethod())))
-                    .toString();
+
 
             byte[] buffer = new byte[4096];
             int bytesRead;
 
-            for(MultipartFile e: imagens) {
+            for (MultipartFile e : imagens) {
 
-                try (
-                        InputStream inputStream = e.getInputStream();
-                        OutputStream outputStream = new FileOutputStream(new File(diretorioUsuario + "\\%d_%d.jpg".formatted(id, random.nextInt(1000, 9999))))
-                ) {
+                File tempFile = new File(String.format("%d_%d",id, random.nextInt(1000, 9999)));
 
-                    while ((bytesRead = inputStream.read(buffer)) > 0) {
-                        outputStream.write(buffer, 0, bytesRead);
-                    }
-                    outputStream.flush();
+                InputStream inputStream = e.getInputStream();
+                OutputStream outputStream = new FileOutputStream(tempFile);
 
-                } catch (IOException ex) {
-                    ex.printStackTrace();
+
+                while ((bytesRead = inputStream.read(buffer)) > 0) {
+                    outputStream.write(buffer, 0, bytesRead);
                 }
+                outputStream.flush();
 
+
+                BlobId blobId = BlobId.of("eletroficina-galvao-storage.appspot.com",
+                        String.format("Imagens/%d/%s/%s", id, this.method, tempFile.getName().concat(e.getOriginalFilename().substring(e.getOriginalFilename().lastIndexOf(".")))));
+                //TODO: reconhecer o jpg
+                BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("image/png").build();
+                storage.create(blobInfo, Files.readAllBytes(tempFile.toPath()));
+
+                //TODO: ajeitar a url
+                String URL = "https://firebasestorage.googleapis.com/v0/b/eletroficina-galvao-storage.appspot.com/o/%s?alt=media".formatted(URLEncoder.encode(tempFile.getName(), StandardCharsets.UTF_8));
+                tempFile.delete();
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return diretorioUsuario;
+        //TODO: retornar a url da pasta generalizada
+        return "aa";
     }
 
     private void setMethod(int method) {
@@ -120,13 +127,6 @@ public class ImageService {
 //        return zipTemp;
 //    }
 
-    public void delete(int id){
-        try {
-            String a =  diretorio + "/OrdensDeServicos/ImagensOS/%s".formatted(id);
-
-            Files.delete(Path.of(a));
-        } catch (IOException e){
-            e.printStackTrace();
-        }
+    public void delete(int id) {
     }
 }
